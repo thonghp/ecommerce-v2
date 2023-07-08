2b45b1deaf1b67c873199474742a71d637068bcf
thonghp <thonghoangpham123@gmail.com>
2023-07-08 16:31:40 +0700

    code product entity, repository and unit test

---
+++ Common/src/main/java/com/hpt/common/entity/Product.java
@@ -0,0 +1,69 @@
+package com.hpt.common.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique = true, length = 256, nullable = false)
+    private String name;
+
+    @Column(unique = true, length = 256, nullable = false)
+    private String alias;
+
+    @Column(length = 512, nullable = false, name = "short_description")
+    private String shortDescription;
+
+    @Column(length = 4096, nullable = false, name = "full_description")
+    private String fullDescription;
+
+    @Column(name = "created_time")
+    private Date createdTime;
+
+    @Column(name = "updated_time")
+    private Date updatedTime;
+
+    private boolean enabled;
+
+    @Column(name = "in_stock")
+    private boolean inStock;
+
+    private float cost;
+
+    private float price;
+
+    @Column(name = "discount_percent")
+    private float discountPercent;
+
+    private float length;
+    private float width;
+    private float height;
+    private float weight;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @ManyToOne
+    @JoinColumn(name = "brand_id")
+    private Brand brand;
+
+    @Override
+    public String toString() {
+        return "Product{" + "id=" + id + ", name='" + name + '\'' + '}';
+    }
+}
---
+++ WebParent/Backend/src/main/java/com/hpt/backend/product/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.hpt.backend.product;
+
+import com.hpt.common.entity.Product;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends PagingAndSortingRepository<Product, Integer> {
+}
