af80c225a5a5e2705f25836c795dcd72957ab540
thonghp <thonghoangpham123@gmail.com>
2023-08-19 23:54:35 +0700

    code login with facebook

--- WebParent/Frontend/src/main/java/com/hpt/frontend/oauth/CustomerOAuth2UserService.java
+++ WebParent/Frontend/src/main/java/com/hpt/frontend/oauth/CustomerOAuth2UserService.java
@@ -10,7 +10,8 @@ import org.springframework.stereotype.Service;
 public class CustomerOAuth2UserService extends DefaultOAuth2UserService {
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        String clientName = userRequest.getClientRegistration().getClientName();
         OAuth2User oAuth2User = super.loadUser(userRequest);
-        return new CustomerOAuth2User(oAuth2User);
+        return new CustomerOAuth2User(oAuth2User, clientName);
     }
 }
