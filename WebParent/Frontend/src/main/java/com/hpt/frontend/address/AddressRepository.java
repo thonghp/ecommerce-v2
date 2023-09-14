f6f07d12b50d72db7389cae38dd90eb0ecce0480
thonghp <thonghoangpham123@gmail.com>
2023-09-14 18:15:15 +0700

    code address account

---
+++ WebParent/Frontend/src/main/java/com/hpt/frontend/address/AddressRepository.java
@@ -0,0 +1,22 @@
+package com.hpt.frontend.address;
+
+import com.hpt.common.entity.Address;
+import com.hpt.common.entity.Customer;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AddressRepository extends CrudRepository<Address, Integer> {
+    List<Address> findByCustomer(Customer customer);
+
+    @Query("SELECT a FROM Address a WHERE a.id = ?1 AND a.customer.id = ?2")
+    Address findByIdAndCustomer(Integer addressId, Integer customerId);
+
+    @Query("DELETE FROM Address a WHERE a.id = ?1 AND a.customer.id = ?2")
+    @Modifying
+    void deleteByIdAndCustomer(Integer addressId, Integer customerId);
+}
